836953ec4b57bef1d5797d3f28ce31459b5b1236
emrahugurlu2003 <emrahugurlu2023@gmail.com>
2023-07-04 20:11:41 +0300

    Bitik

--- horoscope/src/App.js
+++ horoscope/src/App.js
@@ -1,16 +1,22 @@
+
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Navbar from "./components/navbar/Navbar";
-import data from "./helper/data";
-import "./app.scss";
+import  data from "./helper/data"
+
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Header />
-      <Main />
-    </>
-  );
+  return (<>
+
+   <Navbar/>
+   <Header/>
+   <Main/>
+
+
+  </>
+ 
+  )
+  
+  
 }
 
 export default App;
--- horoscope/src/components/navbar/Navbar.js
+++ horoscope/src/components/navbar/Navbar.js
@@ -1,33 +1,9 @@
-import "./Navbar.scss";
-import Header from "../header/Header";
-import Main from "../main/Main";
-import logo from "../../helper/logo/logo.png";
+
 
 const Navbar = () => {
   return (
-    <div className="NavBar">
-      <img src="" alt="" />
-      <div>
-        <ul>
-          <li>
-            <a href="#">HOROSCOPE</a>
-          </li>
-          <li>
-            <a href="#">DAILY</a>
-          </li>
-          <li>
-            <a href="#">TAROT</a>
-          </li>
-          <li>
-            <a href="#">ARTICLE</a>
-          </li>
-          <li>
-            <a href="#">CONTACT</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+    <div>Navbar</div>
+  )
+}
 
-export default Navbar;
+export default Navbar
